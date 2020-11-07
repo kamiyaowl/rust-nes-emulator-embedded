7c40150fa579851f1e4851a60e71a012c5cbf2cc
Kamiya <kamiyaowl@gmail.com>
2020-11-07 12:43:45 +0900

    feat: emulate NES on GUI (#8)
    * feat: run on raylib
    
    * feat: Dockerでのビルドもraylibに対応

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,30 @@
 FROM rust:1.47.0-buster
+ENV DEBIAN_FRONTEND noninteractive
 
 RUN rustup install nightly
 
-RUN apt-get update \
-  && apt-get install -y \
+RUN apt update \
+  && apt install -y --no-install-recommends \
   git \
   libgtk-3-dev \
   gcc \
   g++ \
-  gcc-arm-none-eabi
+  build-essential \
+  libasound2-dev \
+  mesa-common-dev \
+  libx11-dev \
+  libxrandr-dev \
+  libxi-dev \
+  xorg-dev \
+  libgl1-mesa-dev \
+  libglu1-mesa-dev \
+  gcc-arm-none-eabi \
+  && apt -y clean
+
+RUN git clone https://github.com/raysan5/raylib.git raylib \
+  && cd raylib/src/ \
+  && make PLATFORM=PLATFORM_DESKTOP \
+  && make install
 
 RUN mkdir /work
 WORKDIR /work
--- minimal/main.cpp
+++ minimal/main.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 #include <cstdint>
 
+#include "raylib.h"
 #include "rust_nes_emulator_embedded.h"
 
 uint8_t fb[EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT][EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH][EMBEDDED_EMULATOR_NUM_OF_COLOR];
 
-void print_framebuffer() {
-    std::cout << "#print_framebuffer()" << std::endl;
-
-    for(uint32_t j = 0 ; j < EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT ; ++j) {
-        for(uint32_t i = 0 ; i < EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH ; ++i) {
-            if ((fb[j][i][0] == 0) && (fb[j][i][1] == 0) && (fb[j][i][2] == 0)) {
-                std::cout << ".";
-            } else {
-                std::cout << "#";
-            }
-        }
-        std::cout << std::endl;
-    }
-}
-int main(void) {
+int main(int argc, char* argv[])
+{
     // Emulator initialize
     EmbeddedEmulator_init();
     if (EmbeddedEmulator_load() == false) {
@@ -27,9 +15,43 @@ int main(void) {
         return 1;
     }
 
-    // Emulator run
-    EmbeddedEmulator_update_screen(&fb);
-    print_framebuffer();
+    // Screen Initialize
+    const uint32_t scale = 2;
+    const uint32_t screenWidth = EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH * scale;
+    const uint32_t screenHeight = EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT * scale;
+    const uint32_t fps = 60;
+
+    InitWindow(screenWidth, screenHeight, "rust-nes-emulator-embedded");
+    SetTargetFPS(fps);
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        EmbeddedEmulator_update_screen(&fb);
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(BLACK);
+        for(uint32_t j = 0 ; j < EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT ; ++j) {
+            for(uint32_t i = 0 ; i < EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH ; ++i) {
+                Color c = {
+                    .r = fb[j][i][0],
+                    .g = fb[j][i][1],
+                    .b = fb[j][i][2],
+                    .a = 255,
+                };
+                const uint32_t x = i * scale;
+                const uint32_t y = j * scale;
+                DrawRectangle(x, y, scale, scale, c);
+            }
+        }
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    // Finalize
+    CloseWindow();
 
     return 0;
 }
