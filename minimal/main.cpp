163f8b3bef6718421a625269f60454ce9eb688c6
K <kamiyaowl@gmail.com>
2020-11-07 22:32:16 +0900

    feat: impl screen resize

--- minimal/main.cpp
+++ minimal/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <cstdlib>
 
 #include "raylib.h"
 #include "rust_nes_emulator_embedded.h"
@@ -16,13 +17,15 @@ int main(int argc, char* argv[])
     }
 
     // Screen Initialize
-    const uint32_t scale = 2;
-    const uint32_t screenWidth = EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH * scale;
-    const uint32_t screenHeight = EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT * scale;
-    const uint32_t fps = 60;
+    const float scale = 2;
+    const uint32_t screenWidth = static_cast<int>(EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH * scale);
+    const uint32_t screenHeight = static_cast<int>(EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT * scale);
+    const uint32_t fps = 0;
 
     InitWindow(screenWidth, screenHeight, "rust-nes-emulator-embedded");
-    SetTargetFPS(fps);
+    if (fps > 0) {
+        SetTargetFPS(fps);
+    }
 
     // FrameBuffer Image
     Image fbImg = { fb, EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH, EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH, 1, UNCOMPRESSED_R8G8B8A8 };
@@ -31,22 +34,23 @@ int main(int argc, char* argv[])
     // Main game loop
     while (!WindowShouldClose())
     {
-        // Update
+        // Emulate and update framebuffer
         EmbeddedEmulator_update_screen(&fb);
-        Color* fbPtr = reinterpret_cast<Color*>(fb);
+        Color* fbPtr = reinterpret_cast<Color*>(fb); // Color* fbPtr = GetImageData(fbImg); free(fbPtr);
         UpdateTexture(fbTexture, fbPtr);
 
         // Draw
         BeginDrawing();
         {
-            ClearBackground(BLACK);
-            DrawTexture(fbTexture, 0, 0, WHITE);
-            DrawFPS(10, screenHeight - 20);
+            DrawTextureEx(fbTexture, Vector2{ 0, 0 }, 0, scale, WHITE);
+            DrawFPS(10, 10);
         }
         EndDrawing();
     }
 
     // Finalize
+    UnloadTexture(fbTexture);
+    UnloadImage(fbImg);
     CloseWindow();
 
     return 0;
