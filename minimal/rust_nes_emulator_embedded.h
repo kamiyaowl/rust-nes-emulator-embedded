404868c4b2da3c6a318168e84f13944895313a5e
K <kamiyaowl@gmail.com>
2020-11-09 01:18:44 +0900

    Define functions that do not use EmbeddedEmulator.

--- minimal/rust_nes_emulator_embedded.h
+++ minimal/rust_nes_emulator_embedded.h
@@ -9,6 +9,14 @@ static const uintptr_t EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT = 240;
 
 static const uintptr_t EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH = 256;
 
+enum class CpuInterrupt : uint8_t {
+  NMI,
+  RESET,
+  IRQ,
+  BRK,
+  NONE,
+};
+
 enum class KeyEvent : uint8_t {
   PressA,
   PressB,
@@ -29,6 +37,55 @@ enum class KeyEvent : uint8_t {
 };
 
 extern "C" {
+
+/// CPUを1cycエミュレーションします
+uint8_t EmbeddedEmulator_EmulateCpu(uint8_t *raw_cpu_ptr, uint8_t *raw_system_ptr);
+
+/// PPUをエミュレーションします。cpu cycを基準に進めます
+/// `cpu_cyc`: cpuでエミュレーション経過済で、PPU側に未反映のCPU Cycle数合計
+CpuInterrupt EmbeddedEmulator_EmulatePpu(uint8_t *raw_ppu_ptr,
+                                         uint8_t *raw_system_ptr,
+                                         uintptr_t cpu_cycle,
+                                         uint8_t (*fb)[EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT][EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH][EMBEDDED_EMULATOR_NUM_OF_COLOR]);
+
+/// 画面全体を描画するのに必要なCPU Cylceを返します
+uintptr_t EmbeddedEmulator_GetCpuCyclePerFrame();
+
+/// Cpuのデータ構造に必要なサイズを返します
+uintptr_t EmbeddedEmulator_GetCpuDataSize();
+
+/// Ppuのデータ構造に必要なサイズを返します
+uintptr_t EmbeddedEmulator_GetPpuDataSize();
+
+/// SubSystemのデータ構造に必要なサイズを返します
+uintptr_t EmbeddedEmulator_GetSystemDataSize();
+
+/// Cpuの構造体を初期化します
+void EmbeddedEmulator_InitCpu(uint8_t *raw_ptr);
+
+/// Ppuの構造体を初期化します
+void EmbeddedEmulator_InitPpu(uint8_t *raw_ptr);
+
+/// Systemの構造体を初期化します
+void EmbeddedEmulator_InitSystem(uint8_t *raw_ptr);
+
+/// CPUに特定の割り込みを送信します
+void EmbeddedEmulator_InterruptCpu(uint8_t *raw_cpu_ptr,
+                                   uint8_t *raw_system_ptr,
+                                   CpuInterrupt interrupt);
+
+/// ROMを読み込みます
+/// 成功した場合はtrueが返ります。実行中のエミュレートは中止して、Resetをかけてください
+bool EmbeddedEmulator_LoadRom(uint8_t *raw_system_ptr,
+                              const uint8_t *rom_ptr);
+
+/// エミュレータをリセットします
+/// 各種変数の初期化後、RESET割り込みが行われます
+void EmbeddedEmulator_Reset(uint8_t *raw_cpu_ptr, uint8_t *raw_system_ptr, uint8_t *raw_ppu_ptr);
+
+/// キー入力を反映
+/// `player_num` - Player番号, 0 or 1
+void EmbeddedEmulator_UpdateKey(uint8_t *raw_system_ptr, uint32_t player_num, KeyEvent key);
 
 void EmbeddedEmulator_init();
 
--- minimal/src/lib.rs
+++ minimal/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(lang_items, core_intrinsics)]
 
 use core::intrinsics;
+use core::mem;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -42,6 +43,189 @@ pub enum KeyEvent {
     ReleaseDown,
     ReleaseLeft,
     ReleaseRight,
+}
+
+#[repr(u8)]
+pub enum CpuInterrupt {
+    NMI,
+    RESET,
+    IRQ,
+    BRK,
+    NONE,
+}
+
+/// 配列への参照を構造体への参照に変換します
+/// `raw_ptr` - 構造体の参照先。 ARM向けを考慮すると、4byte alignした位置に配置されていることが望ましい
+unsafe fn get_struct_ptr<T>(raw_ptr: &mut u8) -> &mut T {
+    core::mem::transmute::<&mut u8, &mut T>(raw_ptr)
+}
+
+/// 配列への参照に対し、特定の構造体とみなして初期化します
+/// `raw_ptr` - 構造体の参照先。 ARM向けを考慮すると、4byte alignした位置に配置されていることが望ましい
+unsafe fn init_struct_ptr<T: Default>(raw_ptr: &mut u8) {
+    let data_ptr = get_struct_ptr::<T>(raw_ptr);
+    *data_ptr = T::default();
+}
+
+/// 画面全体を描画するのに必要なCPU Cylceを返します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_GetCpuCyclePerFrame() -> usize {
+    CYCLE_PER_DRAW_FRAME
+}
+
+/// Cpuのデータ構造に必要なサイズを返します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_GetCpuDataSize() -> usize {
+    mem::size_of::<Cpu>()
+}
+
+/// SubSystemのデータ構造に必要なサイズを返します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_GetSystemDataSize() -> usize {
+    mem::size_of::<System>()
+}
+
+/// Ppuのデータ構造に必要なサイズを返します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_GetPpuDataSize() -> usize {
+    mem::size_of::<Ppu>()
+}
+
+/// Cpuの構造体を初期化します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_InitCpu(raw_ptr: &mut u8) {
+    init_struct_ptr::<Cpu>(raw_ptr);
+}
+
+/// Systemの構造体を初期化します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_InitSystem(raw_ptr: &mut u8) {
+    init_struct_ptr::<System>(raw_ptr);
+}
+
+/// Ppuの構造体を初期化します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_InitPpu(raw_ptr: &mut u8) {
+    init_struct_ptr::<Ppu>(raw_ptr);
+}
+
+/// CPUに特定の割り込みを送信します
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_InterruptCpu(
+    raw_cpu_ptr: &mut u8,
+    raw_system_ptr: &mut u8,
+    interrupt: CpuInterrupt,
+) {
+    let cpu_ptr = get_struct_ptr::<Cpu>(raw_cpu_ptr);
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+    let irq = match interrupt {
+        CpuInterrupt::NMI => Interrupt::NMI,
+        CpuInterrupt::RESET => Interrupt::RESET,
+        CpuInterrupt::IRQ => Interrupt::IRQ,
+        CpuInterrupt::BRK => Interrupt::BRK,
+        _ => Interrupt::RESET, // とりあえずResetにしておく
+    };
+    (*cpu_ptr).interrupt(&mut *system_ptr, irq);
+}
+
+/// エミュレータをリセットします
+/// 各種変数の初期化後、RESET割り込みが行われます
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_Reset(
+    raw_cpu_ptr: &mut u8,
+    raw_system_ptr: &mut u8,
+    raw_ppu_ptr: &mut u8
+) {
+    let cpu_ptr = get_struct_ptr::<Cpu>(raw_cpu_ptr);
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+    let ppu_ptr = get_struct_ptr::<Ppu>(raw_ppu_ptr);
+    (*cpu_ptr).reset();
+    (*system_ptr).reset();
+    (*ppu_ptr).reset();
+    (*cpu_ptr).interrupt(&mut *system_ptr, Interrupt::RESET);
+}
+
+/// ROMを読み込みます
+/// 成功した場合はtrueが返ります。実行中のエミュレートは中止して、Resetをかけてください
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_LoadRom(
+    raw_system_ptr: &mut u8,
+    rom_ptr: *const u8,
+) -> bool {
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+    (*system_ptr)
+        .cassette
+        .from_ines_binary(|addr: usize| *rom_ptr.offset(addr as isize))
+}
+
+/// CPUを1cycエミュレーションします
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_EmulateCpu(
+    raw_cpu_ptr: &mut u8,
+    raw_system_ptr: &mut u8,
+) -> u8 {
+    let cpu_ptr = get_struct_ptr::<Cpu>(raw_cpu_ptr);
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+
+    (*cpu_ptr).step(&mut (*system_ptr))
+}
+
+/// PPUをエミュレーションします。cpu cycを基準に進めます
+/// `cpu_cyc`: cpuでエミュレーション経過済で、PPU側に未反映のCPU Cycle数合計
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_EmulatePpu(
+    raw_ppu_ptr: &mut u8,
+    raw_system_ptr: &mut u8,
+    cpu_cycle: usize,
+    fb: &mut [[[u8; EMBEDDED_EMULATOR_NUM_OF_COLOR]; EMBEDDED_EMULATOR_VISIBLE_SCREEN_WIDTH];
+             EMBEDDED_EMULATOR_VISIBLE_SCREEN_HEIGHT],
+) -> CpuInterrupt {
+    let ppu_ptr = get_struct_ptr::<Ppu>(raw_ppu_ptr);
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+
+    match (*ppu_ptr).step(cpu_cycle, &mut (*system_ptr), fb) {
+        Some(Interrupt::NMI) => CpuInterrupt::NMI,
+        Some(Interrupt::RESET) => CpuInterrupt::RESET,
+        Some(Interrupt::IRQ) => CpuInterrupt::IRQ,
+        Some(Interrupt::BRK) => CpuInterrupt::BRK,
+        None => CpuInterrupt::NONE,
+    }
+}
+
+/// キー入力を反映
+/// `player_num` - Player番号, 0 or 1
+#[no_mangle]
+pub unsafe extern "C" fn EmbeddedEmulator_UpdateKey(
+    raw_system_ptr: &mut u8,
+    player_num: u32,
+    key: KeyEvent,
+) {
+    let system_ptr = get_struct_ptr::<System>(raw_system_ptr);
+    let p = match player_num {
+        0 => &mut (*system_ptr).pad1,
+        1 => &mut (*system_ptr).pad2,
+        _ => &mut (*system_ptr).pad1, // とりあえず1Pにしておく
+    };
+
+    match key {
+        KeyEvent::PressA => p.push_button(PadButton::A),
+        KeyEvent::PressB => p.push_button(PadButton::B),
+        KeyEvent::PressSelect => p.push_button(PadButton::Select),
+        KeyEvent::PressStart => p.push_button(PadButton::Start),
+        KeyEvent::PressUp => p.push_button(PadButton::Up),
+        KeyEvent::PressDown => p.push_button(PadButton::Down),
+        KeyEvent::PressLeft => p.push_button(PadButton::Left),
+        KeyEvent::PressRight => p.push_button(PadButton::Right),
+
+        KeyEvent::ReleaseA => p.release_button(PadButton::A),
+        KeyEvent::ReleaseB => p.release_button(PadButton::B),
+        KeyEvent::ReleaseSelect => p.release_button(PadButton::Select),
+        KeyEvent::ReleaseStart => p.release_button(PadButton::Start),
+        KeyEvent::ReleaseUp => p.release_button(PadButton::Up),
+        KeyEvent::ReleaseDown => p.release_button(PadButton::Down),
+        KeyEvent::ReleaseLeft => p.release_button(PadButton::Left),
+        KeyEvent::ReleaseRight => p.release_button(PadButton::Right),
+    };
 }
 
 pub struct EmbeddedEmulator {
