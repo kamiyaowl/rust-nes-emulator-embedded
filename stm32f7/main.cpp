ab8ffd8adba475d27d38ff3a405f592f6d516e13
K <kamiyaowl@gmail.com>
2020-11-18 03:38:55 +0900

    (wip) show hexdump

--- stm32f7/main.cpp
+++ stm32f7/main.cpp
@@ -20,11 +20,11 @@
 MBED_ALIGN(32) static uint8_t emuWorkBuffer[EMU_WORK_SIZE]; // MBED_SECTION(".emu_work");
 
 // user specified values
-#define PRINT_MESSAGE_HEIGHT (20)
-#define SDRAM_BASE_ADDR      (SDRAM_DEVICE_ADDR)
-#define SDRAM_SIZE           (SDRAM_DEVICE_SIZE)
-#define DISPLAY_WIDTH        (OTM8009A_800X480_WIDTH)  // 800
-#define DISPLAY_HEIGHT       (OTM8009A_800X480_HEIGHT) // 480
+#define PRINT_MESSAGE_HEIGHT          (20)
+#define SDRAM_BASE_ADDR               (SDRAM_DEVICE_ADDR)       //  0xc000_0000
+#define SDRAM_SIZE                    (SDRAM_DEVICE_SIZE)       // ~0xc100_0000(16MByte)
+#define DISPLAY_WIDTH                 (OTM8009A_800X480_WIDTH)  // 800
+#define DISPLAY_HEIGHT                (OTM8009A_800X480_HEIGHT) // 480
 
 void initSystem(void) {
     // Enabled I/D Cache, Prefetch Buffer
@@ -57,7 +57,21 @@ bool initSd(void) {
     return (BSP_SD_Init() == MSD_OK);
 }
 int main(void) {
-    char msg[64];
+    // Allocate SDRAM Buffer
+    // Frame Buffer:
+    // - [0xc000_0000 - 0xc017_6fff]
+    // - [0xc017_7000 - 0xc02e_dfff]
+    // General Buffer:
+    // - [0xc02e_e000 - ]
+    const uint32_t frameBufferSize   = (DISPLAY_WIDTH * DISPLAY_HEIGHT * EMBEDDED_EMULATOR_NUM_OF_COLOR); // 138800byte
+    uint8_t* frameBuffer0Ptr   = reinterpret_cast<uint8_t*>(SDRAM_BASE_ADDR);
+    uint8_t* frameBuffer1Ptr   = frameBuffer0Ptr + frameBufferSize;
+
+    uint8_t* generalBufferPtr  = frameBuffer1Ptr + frameBufferSize;
+    const uint32_t generalBufferSize = (SDRAM_SIZE - reinterpret_cast<uint32_t>(generalBufferPtr));
+
+    // work data
+    char msg[128];
     uint32_t messageLine = 0;
 
     // Init core peripheral
@@ -89,6 +103,28 @@ int main(void) {
         BSP_LCD_DisplayStringAt(0, (messageLine++ * PRINT_MESSAGE_HEIGHT), (uint8_t *)"[ERROR] FAILED", LEFT_MODE);
         while(1);
     }   
+    
+    // Read binary from SD Card
+    // TODO: FATFS support
+    BSP_LCD_DisplayStringAt(0, (messageLine++ * PRINT_MESSAGE_HEIGHT), (uint8_t *)"[INFO ] Read ROM from SD", LEFT_MODE);
+    if (MSD_OK != BSP_SD_ReadBlocks(reinterpret_cast<uint32_t*>(generalBufferPtr), 0x0, 0x1, 10)) {
+        BSP_LCD_DisplayStringAt(0, (messageLine++ * PRINT_MESSAGE_HEIGHT), (uint8_t *)"[ERROR] FAILED", LEFT_MODE);
+        while(1);
+    }
+    // Show HexDump
+    for (uint32_t i = 0; i < 0x10; i++) {
+        const uint32_t offset = i * 0x10;
+        sprintf(
+            msg, 
+            "%08x: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x ", 
+            generalBufferPtr + offset,
+            generalBufferPtr[offset + 0x00], generalBufferPtr[offset + 0x01], generalBufferPtr[offset + 0x02], generalBufferPtr[offset + 0x03], 
+            generalBufferPtr[offset + 0x04], generalBufferPtr[offset + 0x05], generalBufferPtr[offset + 0x06], generalBufferPtr[offset + 0x07], 
+            generalBufferPtr[offset + 0x08], generalBufferPtr[offset + 0x09], generalBufferPtr[offset + 0x0a], generalBufferPtr[offset + 0x0b], 
+            generalBufferPtr[offset + 0x0c], generalBufferPtr[offset + 0x0d], generalBufferPtr[offset + 0x0e], generalBufferPtr[offset + 0x0f]
+        );
+        BSP_LCD_DisplayStringAt(0, (messageLine++ * PRINT_MESSAGE_HEIGHT), (uint8_t *)msg, LEFT_MODE);
+    }
 
     // Init emulator
     while(1);
